97a36c88a12f03e1da83fbcde86f1950faee959b
Andrew Ebersole <acebersole@gmail.com>
2023-07-26 11:46:28 -0400

    Fixed and error in headers

--- UndeadKnights/Tiles/TileManager.cs
+++ UndeadKnights/Tiles/TileManager.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// ---------------------------------------------------------------- //
+// Collaborators | Andrew Ebersole
+// Created Date  | 7-26-23
+// Last Update   | 7-26-23
+// Purpose       | Manages all the tiles and upgrades them when needed
+// ---------------------------------------------------------------- //
+
 namespace UndeadKnights.Tiles
 {
     internal class TileManager
     {
+        // --- Fields --- //
+
+
+
+
+
+        // --- Properties --- //
+
+
+
+
+
+        // --- Constructor --- //
+
+
+
+
+
+        // --- Methods --- // 
+
+        /// <summary>
+        /// Called every frame to update functions
+        /// </summary>
+        /// <param name="gt"></param>
+        public void Update(GameTime gt)
+        {
+
+        }
+
+        /// <summary>
+        /// Called every frame to update graphical elements
+        /// </summary>
+        /// <param name="sb"></param>
+        public void Draw(SpriteBatch sb)
+        {
+
+        }
     }
 }
