21ae17e8ef632cc86fbaa83df7a177c8bbf4acb1
Andrew Ebersole <acebersole@gmail.com>
2023-09-16 12:58:09 -0400

    Created tutorial file

---
+++ UndeadKnights/Tutorial.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ---------------------------------------------------------------- //
+// Collaborators | Andrew Ebersole
+// Created Date  | 9-15-23
+// Last Update   | 9-15-23
+// Purpose       | Used to show tips on how to play at different
+//               | Stages in the game
+// ---------------------------------------------------------------- //
+
+namespace UndeadKnights
+{
+    internal class Tutorial
+    {
+        // --- Fields --- //
+
+
+
+
+
+        // --- Properties --- //
+
+
+
+
+
+        // --- Constructor --- //
+
+
+
+
+
+        // --- Methods --- // 
+
+        /// <summary>
+        /// Called every frame to update functions
+        /// </summary>
+        /// <param name="gt"></param>
+        public void Update(GameTime gt)
+        {
+
+        }
+
+        /// <summary>
+        /// Called every frame to update graphical elements
+        /// </summary>
+        /// <param name="sb"></param>
+        public void Draw(SpriteBatch sb)
+        {
+
+        }
+    }
+}
